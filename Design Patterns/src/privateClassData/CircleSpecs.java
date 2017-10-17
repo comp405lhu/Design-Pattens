1218538c0f0b27109da5a5161b34115f60293b6a
Mitch <doublemx2@gmail.com>
2017-10-17 14:45:32 -0400

    Added toString to CircleSpecs

--- Design Patterns/src/privateClassData/CircleSpecs.java
+++ Design Patterns/src/privateClassData/CircleSpecs.java
@@ -4,20 +4,25 @@ import java.awt.Point;
 
 public class CircleSpecs {
 	
-	private double radius;
-	private Point center;
+	private double r;
+	private Point p;
 
 	public double getRadius() {
-		return radius;
+		return r;
 	}
 
 	public Point getCenter() {
-		return center;
+		return p;
 	}
 
 	public CircleSpecs(double radius, Point center) {
-		this.radius = radius;
-		this.center = center;
+		this.r = radius;
+		this.p = center;
+	}
+	
+	@Override
+	public String toString() {
+		return "Circle(radius: " + r + ", center: " + p + ");";
 	}
 
 }
