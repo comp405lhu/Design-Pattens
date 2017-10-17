0c1029e164e51830e4972a6bc30971506ecb4a7d
kpillai <kpillai@vm-akel6419.lhup.edu>
2017-10-17 15:13:27 -0400

    fixed errors

--- Design Patterns/src/privateClassData/CircleSpecs.java
+++ Design Patterns/src/privateClassData/CircleSpecs.java
@@ -3,7 +3,7 @@ package privateClassData;
 import java.awt.Point;
 
 public class CircleSpecs {
-	
+
 	private double radius;
 	private Point center;
 
@@ -19,16 +19,17 @@ public class CircleSpecs {
 		this.radius = radius;
 		this.center = center;
 	}
-	
+
 	@Override
 	public String toString() {
-		String str ="";
-			while (i < 20) {
-			
+		String str = "";
+		int i = 0;
+		while (i < 20) {
+
 			str += i;
 			i++;
 		}
-		return radius+":"+"("+center.x+","+center.y+")" + "-"+ str;
+		return radius + ":" + "(" + center.x + "," + center.y + ")" + "-" + str;
 	}
 
 }
